a39a968ce09cf0db9cf90b55066690c1bf7a9750
Vojda <rasparuhov@gmail.com>
2015-11-21 19:08:29 +0200

    zig-zag, but working

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,27 +5,128 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.PriorityQueue;
 
 public class Main {
 
+	static String path = "C:\\Users\\Vojda\\Downloads\\map1.csv";
+	static int startX = 0;
+	static int startY = 5;
+	static int endX = 7;
+	static int endY = 4;
+	static ArrayList<ArrayList<Character>> mazeList = readFromFile(path);
+	static int length = mazeList.size();
+	static int width = mazeList.get(0).size();
+	static ArrayList<Node> visited = new ArrayList<Node>();
+	
+	static char[][] maze = transformToArray(mazeList);
+	
+	static PriorityQueue<Node> queue = new PriorityQueue<Node>();
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void main(String[] args) {
-		ArrayList<ArrayList<Character>> maze = readFromFile();
+//		Maze maze = new Maze(args[0]);
+//		Point start = new Point(args[1], args[2]));
+//		Point end = new Point(args[3], args[4]));
+//		
+//		List<Point> path = maze.findPath(start, end);
+//		
+//		for (Point step : path) {
+//    			System.out.print(step.toString());
+//
+//    			if (!step.equals(end)) {
+//    				System.out.print(" ,");
+//    			}	
+//		}
+
+//		String path = args[0];
 		
-		for (Iterator iterator = maze.iterator(); iterator.hasNext();) {
-			ArrayList<Character> arrayList = (ArrayList<Character>) iterator
-					.next();
-			for (Iterator iterator2 = arrayList.iterator(); iterator2.hasNext();) {
-				Character character = (Character) iterator2.next();
-				System.out.println(character);
+		double startWeight = calculateweight(startX, startY, 0);
+		
+		Node start = new Node(startX, startY, 0);
+		
+		queue.add(start);
+		
+		while (true){
+			//Return element to trace the path if the end is reached
+			Node current = queue.poll();
+			Node node = null;
+			if (current != null){
+				node = generateChildren(current);
+			}
+			
+			if (node != null){
+				System.out.println(node);
+				while(node.getParrent() != null){
+					node = node.getParrent();
+					System.out.println(node);
+				}
+				break;
 			}
 		}
+
 	}
 
-	private static ArrayList<ArrayList<Character>> readFromFile() {
-		String csvFile = "C:\\Users\\Vojda\\Downloads\\map1.csv";
+	//BEWARE THE COPY-PASTE DRIVEN DEVELOPMENT!!!
+	private static Node generateChildren(Node node) {
+		visited.add(node);
+		for (int i = -1; i < 2; i++){
+			for (int j = -1; j < 2; j++){
+				if ( i!= 0 || j != 0){
+					int newX = node.getX() + i;
+					int newY = node.getY() + j;
+					double newWeight = 1;
+					char field;
+					try {
+						field = maze[newX][newY];
+					} catch (IndexOutOfBoundsException e){break;}
+					if (i != 0 && j != 0){
+						newWeight += 0.5;
+					}
+					if (field == '~'){
+						newWeight += 2;
+					}
+					
+					Node newNode = new Node(newX, newY, calculateweight(newX, newY, newWeight + node.getPathPassed()), node, newWeight);
+					if (!visited.contains(newNode) && (field != 'N')){
+						queue.add(newNode);
+					}
+					if (newNode.getX() == endX && newNode.getY() == endY){
+						return newNode;
+					}
+				}
+			}
+		}
+		
+		return null;
+	}
+
+	private static char[][] transformToArray(
+			ArrayList<ArrayList<Character>> mazeList) {
+		char[][] map = new char[mazeList.get(0).size()][mazeList.size()];
+		for (int i = 0; i < mazeList.size(); i++) {
+			ArrayList<Character> line = mazeList.get(i);
+			for (int j = 0; j < line.size(); j++) {
+				map[j][i] = line.get(j).charValue();
+			}
+		}
+		return map;
+	}
+
+	private static double calculateweight(int startX, int startY, double weight) {
+		double result = modul(endX - startX) + modul(endY - startY) + weight;
+		
+		return result;
+	}
+
+	private static int modul(int i) {
+		if (i < 0){
+			return -i;
+		}
+		return i;
+	}
+
+	private static ArrayList<ArrayList<Character>> readFromFile(String path) {
+		String csvFile = path;
 		BufferedReader br = null;
 		String line = "";
 		String cvsSplitBy = ",";
---
+++ src/main/Node.java
@@ -0,0 +1,85 @@
+package main;
+
+public class Node implements Comparable<Node>{
+	private int x;
+	private int y;
+	private double weight;
+	private double pathPassed; //
+	private Node parrent;
+
+	public Node(int x, int y, double weight) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.weight = weight;
+		this.pathPassed = 0;
+		this.setParrent(null);
+	}
+	
+	public Node(int x, int y, double weight, Node parrent, double currWeight) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.weight = weight;
+		this.pathPassed = parrent.pathPassed + currWeight;
+		this.parrent = parrent;
+	}
+	
+	public double getPathPassed() {
+		return pathPassed;
+	}
+
+	public void setPathPassed(double pathPassed) {
+		this.pathPassed = pathPassed;
+	}
+
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	@Override
+	public int compareTo(Node other) {
+		if (this.weight < other.weight){
+			return 1;
+		} else if (this.weight > other.weight){
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+
+	public Node getParrent() {
+		return parrent;
+	}
+
+	public void setParrent(Node parrent) {
+		this.parrent = parrent;
+	}
+	
+	@Override
+	public String toString() {
+		return "Node [x=" + x + ", y=" + y + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Node other = (Node) obj;
+		return (this.x == other.x) && (this.y == other.y);
+	}
+	
+}
